838a4d858d1003e9e4a1ea08a5e2012100a6e9d7
Nathan Egan <egan.nathan@gmail.com>
2016-10-16 18:11:50 -0600

    added tests and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
 
-root 'application#hello'
+  get 'static_pages/contact'
+
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'static_pages#home'
 
 end
