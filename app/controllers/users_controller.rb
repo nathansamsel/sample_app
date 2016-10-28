cd2cca7fcb52498ecd6767516674e68a4be28ba9
Nathan Egan <egan.nathan@gmail.com>
2016-10-28 13:52:01 -0600

    edit page validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #successfull update
+    else
+      render 'edit'
+    end
+  end
+
+
   private
 
     def user_params
